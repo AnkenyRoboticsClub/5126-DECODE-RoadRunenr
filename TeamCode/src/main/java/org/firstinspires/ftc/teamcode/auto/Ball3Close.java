358b5ba042ad35f8ab6cc561abe0483fdac97952
VanillaVulture <90653499+VanillaVulture@users.noreply.github.com>
2026-02-13 00:15:15 -0600

    Make GREENBOT AprilTag aim assist non-blocking and initialize LL

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Ball3Close.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Ball3Close.java
@@ -4,10 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.subsystem.AutoDrive;
-import org.firstinspires.ftc.teamcode.subsystem.DriveTrain;
 import org.firstinspires.ftc.teamcode.subsystem.ImuUtil;
 import org.firstinspires.ftc.teamcode.subsystem.Shooter;
-import org.firstinspires.ftc.teamcode.subsystem.VisionAlign;
 
 @Autonomous(name = "CloseAuto3B", group = "Auto")
 public class Ball3Close extends LinearOpMode {
@@ -18,12 +16,8 @@ public class Ball3Close extends LinearOpMode {
         AutoDrive drive = new AutoDrive(hardwareMap);
         ImuUtil imu = new ImuUtil(hardwareMap);
         Shooter shooter = new Shooter(hardwareMap);
-        DriveTrain teleDrive = new DriveTrain(hardwareMap); // used for LL movement
-        VisionAlign vision = new VisionAlign(teleDrive, imu);
 
         imu.resetYaw();
-        vision.start(hardwareMap);
-
         telemetry.addLine("LL Auto Ready");
         telemetry.update();
 
@@ -31,40 +25,27 @@ public class Ball3Close extends LinearOpMode {
         if (isStopRequested()) return;
 
         // ---------------------------
-        // 1. BLIND BACKUP (so LL sees tag)
+        // 1. Blind backup while holding heading
         // ---------------------------
-        //drive.driveReverse();
-        //sleep(1500);    // tune this (700–1200 ms works)
-        //drive.stopAll();
-        // Backup while holding heading
         drive.driveStraightInchesHoldHeading(this, imu, -6, 0.10, 0.05, 0.10);
         sleep(550);
         drive.turnToHeadingDegrees(this, imu, 0, 0.15, 1);
-        sleep(1700);
-        // ---------------------------
-        // 4. SHOOT ONE
-        // ---------------------------
-        telemetry.addData("Distance", vision.getDistance());
-        //shooter.shootByDistance(vision.getDistance(), this);
-        shooter.customShoot(630, this);
-        sleep(2000);            // wait for flywheel to stabilize
-        telemetry.addData("RPM:", shooter.getFlywheelRpm());
+        sleep(200);
 
+        // ---------------------------
+        // 2. Spin up and kick first ball
+        // ---------------------------
+        shooter.setFlywheelRpm(630);
+        shooter.waitForAtSpeed(this, 2200);
         shooter.feedOne(this);
 
         // ---------------------------
-        // 5. INTAKE 2nd ball into fly wheel
+        // 3. Wait for recovery then intake next 2
         // ---------------------------
-        sleep(500);
+        shooter.waitForAtSpeed(this, 1200);
         shooter.intake();
-        sleep(3000);
-
-        shooter.feedOne(this);
-
-        // ---------------------------
-        // 5. Move off line
-        // ---------------------------
-
+        sleep(2500);
+        shooter.stopIntake();
 
         shooter.stop();
         drive.stopAll();
